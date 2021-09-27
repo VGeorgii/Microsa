bfa49278e6155bbb0ed1af85cbc02c4bf6e82e3b
VGeorgii <65576385+VGeorgii@users.noreply.github.com>
2021-09-27 14:23:13 -0500

    Update fibseg.py

--- microsa/fibseg.py
+++ microsa/fibseg.py
@@ -1,13 +1,16 @@
-def endPoints(skel):
-    ##
-    # Function returns boolen array that contains True values in the coordinates that fit as the endpoint of the fibers.
+def endPoints (skel):
 
-    # arguments:
-    # - ‘skel’: array with skeletonized fibers
 
-    # function returns:
-    # - 'endp': boolen array that contains True values in the coordinates that fit as the endpoint of the fibers
     ##
+    #Function returns boolen array that contains True values in the coordinates that fit as the endpoint of the fibers. 
+
+    #arguments:
+    #- ‘skel’: array with skeletonized fibers
+
+    #function returns:
+    #- 'endp': boolen array that contains True values in the coordinates that fit as the endpoint of the fibers
+    ##
+
 
     epoint1 = np.array([[0, 0, 0], [0, 1, 0], [0, 1, 0]])
 
@@ -41,6 +44,8 @@ def endPoints(skel):
 
     epoint16 = np.array([[0, 0, 0], [1, 1, 0], [1, 0, 0]])
 
+
+
     endp1 = binary_hit_or_miss(skel, structure1=epoint1)
 
     endp2 = binary_hit_or_miss(skel, structure1=epoint2)
@@ -73,117 +78,160 @@ def endPoints(skel):
 
     endp16 = binary_hit_or_miss(skel, structure1=epoint16)
 
-    endp = endp1 + \
-           endp2 + \
-           endp3 + \
-           endp4 + \
-           endp5 + \
-           endp6 + \
-           endp7 + \
-           endp8 + \
-           endp9 + \
-           endp10 + \
-           endp11 + \
-           endp12 + \
-           endp13 + \
-           endp14 + \
-           endp15 + \
-           endp16
+
+    endp =  endp1 + \
+    endp2 + \
+    endp3 + \
+    endp4 + \
+    endp5 + \
+    endp6 + \
+    endp7 + \
+    endp8 + \
+    endp9 + \
+    endp10 + \
+    endp11 + \
+    endp12 + \
+    endp13 + \
+    endp14 + \
+    endp15 + \
+    endp16
+
 
     return endp
 
 
-def endpoints_storage(skel_labels, nlabels):
-    ###
-    # Function returns array that include cooridnates of fiber endpoints.
 
-    # arguments:
-    # - ‘skel_labels’: array that include labels of skeletonized fibers
-    # - ‘nlabels’: total number of labels
+def endpoints_storage (skel_labels, nlabels):
 
-    # function returns:
-    # - 'endp': array that contains coordinates of each endpoint of the fiber
+
     ###
+    #Function returns array that include cooridnates of fiber endpoints. 
+
+    #arguments:
+    #- ‘skel_labels’: array that include labels of skeletonized fibers
+    #- ‘nlabels’: total number of labels
+
+    #function returns:
+    #- 'endp': array that contains coordinates of each endpoint of the fiber
+    ###
+
 
     endpoints_storage = np.where(endPoints(np.array(skel_labels)) == True)
+
 
     endpoints_list = []
 
     for i in np.arange(nlabels + 1):
+
         endpoints_list.append([])
 
-    for i in np.arange(len(endpoints_storage[0])):
-        endpoints_list[np.array(skel_labels[endpoints_storage[0][i]][endpoints_storage[1][i]])].append(
-            [endpoints_storage[0][i], endpoints_storage[1][i]])
 
+    for i in np.arange(len(endpoints_storage[0])):
+
+        endpoints_list[np.array(skel_labels[endpoints_storage[0][i]][endpoints_storage[1][i]])].append([endpoints_storage[0][i], endpoints_storage[1][i]])
+
+    
     return (endpoints_list)
 
 
-def array_transform(array):
+
+def array_transform (array):
+
+
     ###
-    # Function transformed array where values that are equal to 0 transformed to big numbers to performe graph algorithm.
+    #Function transformed array where values that are equal to 0 transformed to big numbers to performe graph algorithm. 
 
-    # arguments:
-    # - ‘array’: array that include labels of skeletonized fibers
+    #arguments:
+    #- ‘array’: array that include labels of skeletonized fibers
 
-    # function returns:
-    # - 'endp': transformed array where values that are equal to 0 transformed to big numbers
+    #function returns:
+    #- 'endp': transformed array where values that are equal to 0 transformed to big numbers
     ###
 
-    array[array == 0] = 2 * 400000
+    
+    array[array == 0] = 2*400000
+
 
     return (array)
 
 
-def path_list(array, endpoints_list, nlabels):
+
+
+def path_list (array, endpoints_list, nlabels):
+
+
     ###
-    # Function returns array that include labels of pruned (without additional branches) fibers
+    #Function returns array that include labels of pruned (without additional branches) fibers
 
-    # arguments:
-    # - ‘array’: array that include labels of skeletonized fibers
-    # - ‘endpoints_list’: array that contains coordinates of each endpoint of the fiber
-    # - ‘nlabels’: total number of labels
+    #arguments:
+    #- ‘array’: array that include labels of skeletonized fibers
+    #- ‘endpoints_list’: array that contains coordinates of each endpoint of the fiber
+    #- ‘nlabels’: total number of labels
 
-    # function returns:
-    # - 'endp': transformed array where values that are equal to 0 transformed to big numbers
+    #function returns:
+    #- 'endp': transformed array where values that are equal to 0 transformed to big numbers
     ###
-
+    
+    
     path_list = []
 
     for i in np.arange(nlabels):
 
         if len(endpoints_list[i]) > 0:
+
             path = route_through_array(np.array(array),
-                                       endpoints_list[i][np.lexsort(np.array(endpoints_list[i]).T[:])[0]],
-                                       endpoints_list[i][np.lexsort(np.array(endpoints_list[i]).T[:])[-1]],
-                                       fully_connected=True, geometric=True)[0]
+                                    endpoints_list[i][np.lexsort(np.array(endpoints_list[i]).T[:])[0]],
+                                    endpoints_list[i][np.lexsort(np.array(endpoints_list[i]).T[:])[-1]],
+                                    fully_connected=True, geometric=True)[0]
 
             path_list.append(path)
 
-    pruned_array = np.zeros((len(array), len(array[0])))
 
+
+    pruned_array = np.zeros((len(array), len(array[0])))
+    
     path_list = list(map(np.array, path_list))
 
     for i in np.arange(len(path_list)):
-        pruned_array[path_list[i][:, 0], path_list[i][:, 1]] = i + 1
+    
+        pruned_array[path_list[i][:,0],path_list[i][:,1]] = i+1
+
 
     return pruned_array
 
 
-def fibers_executor(gray):
-    ##
-    # Function mainly performs segmentation of fibers and extract parameters that are required for other functions like number of labels, distance between centroids etc.
 
-    # arguments:
-    # image – gray scale image to process
+def fibs_filter (segmented_fibs):
 
-    # function returns:
-    # - 'skeleton': image with skeletonized objects,
-    # - 'distance': distance from skeleton to the edge of original object (radius),
-    # - 'skel_labels_pruned': pruned and labeled skeletonized objects,
-    # - 'props_pruned': properties of labeled objects,
-    # - 'nlabels_pruned': number of labeled objects
+    filtered_fibs = []
+
+    for i in np.arange(len(segmented_fibs)):
+
+        if segmented_fibs[i].area > 3:
+
+            filtered_fibs.append(segmented_fibs[i])
+            
+            
+    return filtered_fibs
+
+
+
+def fibers_executor (gray):
+
     ##
+    #Function mainly performs segmentation of fibers and extract parameters that are required for other functions like number of labels, distance between centroids etc.
+
+    #arguments:
+    #image – gray scale image to process
+
+    #function returns:
+    #- 'skeleton': image with skeletonized objects, 
+    #- 'distance': distance from skeleton to the edge of original object (radius), 
+    #- 'skel_labels_pruned': pruned and labeled skeletonized objects, 
+    #- 'props_pruned': properties of labeled objects, 
+    #- 'nlabels_pruned': number of labeled objects
+    ##
+
 
     skeleton, distance = medial_axis(gray, return_distance=True)
 
@@ -202,8 +250,11 @@ def fibers_executor(gray):
     skel_labels_pruned = label(pruning, neighbors=8)
 
     props_pruned = regionprops(skel_labels_pruned)
+    
+    props_pruned = fibs_filter(props_pruned)
+    
 
-    return {'skeleton': skeleton,
-            'distance': distance,
-            'skel_labels_pruned': skel_labels_pruned,
+    return {'skeleton': skeleton, 
+            'distance': distance, 
+            'skel_labels_pruned': skel_labels_pruned, 
             'props_pruned': props_pruned}
