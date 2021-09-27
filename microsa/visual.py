8fba2d6029d246403068cd3af3010cc0c954532c
VGeorgii <65576385+VGeorgii@users.noreply.github.com>
2021-09-27 14:24:49 -0500

    Update visual.py

--- microsa/visual.py
+++ microsa/visual.py
@@ -1,111 +1,134 @@
-def cell_map(list_of_coords, list_of_types, gray, dilation):
+def cell_map (list_of_coords, list_of_types, gray, dilation):
+    
+    
     ###
-    # Function returns array with marked cells and their type
+    #Function returns array with marked cells and their type
 
-    # arguments:
-    # - cells_coords_list: list with cells coordinates (y,x)
-    # - list_of_types: list with cells types
-    # - gray: original image in gray scale
-    # - dilation: size of dot which indicates cell localization
+    #arguments:
+    #- cells_coords_list: list with cells coordinates (y,x)
+    #- list_of_types: list with cells types
+    #- gray: original image in gray scale
+    #- dilation: size of dot which indicates cell localization
 
-    # function returns:
+    #function returns:
     # - array with marked cells and their type
     ###
-
-    array = np.zeros((len(gray), len(gray[0])))
-
+    
+    
+    array = np.zeros((len(gray),len(gray[0])))
+    
     score = 0
-
+    
     indexis = []
-
+    
     for i in np.arange(len(list_of_types)):
+        
         index = []
-
+        
         indexis.append(index)
-
+    
     for i in set(list_of_types):
-
+        
         for j in np.arange(len(list_of_types)):
-
+            
             if list_of_types[j] == i:
+                
                 indexis[j] = score
-
+        
         score += 1
-
-    list_of_coords_types = np.concatenate((list_of_coords, [[i] for i in indexis]), axis=1)
-
-    for i in list_of_coords_types[:, 2]:
-        array[list_of_coords_types[list_of_coords_types[:, 2] == i][:, 0], list_of_coords_types[
-                                                                               list_of_coords_types[:, 2] == i][:,
-                                                                           1]] = i + 2
-
+        
+    
+    list_of_coords_types = np.concatenate((list(list_of_coords), [[i] for i in indexis]), axis = 1)   
+    
+    
+    for i in list_of_coords_types[:,2]:
+        
+        
+        array[list_of_coords_types[list_of_coords_types[:,2] == i][:,0], list_of_coords_types[list_of_coords_types[:,2] == i][:,1]] = i+2
+        
+        
     bigger_points = ndimage.grey_dilation(array, size=(5, 5), structure=np.ones((dilation, dilation)))
-
+    
+    
+    
     return (bigger_points)
 
 
-def fibs_map(array):
+
+
+def fibs_map (array):
+    
     ###
-    # Function returns array that contains fibers skeleton
+    #Function returns array that contains fibers skeleton
 
-    # arguments:
-    # - array: array with labeled fibers
+    #arguments:
+    #- array: array with labeled fibers
 
-    # function returns:
+    #function returns:
     # - array that contains fibers skeleton
     ###
-
+    
     fib_map = np.multiply(array > 0, 1)
-
+    
     return (fib_map)
 
 
-def pict(array, size, colorlist=['black', 'red', 'green', 'yellow', 'orange', 'purple']):
+
+def pict (array, size, colorlist = ['black', 'red', 'green', 'yellow', 'blue', 'purple']):
+    
     ###
-    # Function visualize combined cellular and fibrilar maps
+    #Function visualize combined cellular and fibrilar maps 
 
-    # arguments:
-    # - array: array to be visualized
-    # - size: desired size of image in pixels
-    # - colorlist: list of colors that will be used to indicate different types of cells
+    #arguments:
+    #- array: array to be visualized
+    #- size: desired size of image in pixels
+    #- colorlist: list of colors that will be used to indicate different types of cells
 
-    # function returns:
+    #function returns:
     # - visualized array with marked cells and fibers
     ###
-
+    
     color = matplotlib.colors.ListedColormap(colorlist)
-
-    fig, ax = plt.subplots(figsize=(size, size))
-
-    ax = plt.imshow(array, cmap=color)
-
+    
+    fig, ax = plt.subplots(figsize = (size,size))
+    
+    ax = plt.imshow(array, cmap = color)
+    
     plt.axis('off')
+    
+    
+    return(plt.show())
 
-    return (plt.show())
 
 
-def visualization(image, list_of_coords, cell_type_list, executed_fibs, dilation, size=100):
+def visualization (image, list_of_coords, cell_type_list, executed_fibs, dilation = 3, size = 100):
+    
     ###
-    # Function visualize combined cellular and fibrillar maps
+    #Function visualize combined cellular and fibrillar maps 
 
-    # arguments:
-    # - image: original image in gray scale
-    # - list_of_coords: list with cells coordinates (y,x)
-    # - cell_type_list: list with cells types
-    # - executed_fibs: dictionary executed with fiber_executer function that contains information about segmented fibers
-    # - dilation: size of dot which indicates cell localization
-    # - size: default 100, desired size of image in pixels
+    #arguments:
+    #- image: original image in gray scale
+    #- list_of_coords: list with cells coordinates (y,x)
+    #- cell_type_list: list with cells types
+    #- executed_fibs: dictionary executed with fiber_executer function that contains information about segmented fibers
+    #- dilation: size of dot which indicates cell localization
+    #- size: default 100, desired size of image in pixels
 
-    # function returns:
+    #function returns:
     # - visualized array with marked cells and fibers
     ###
-
-    cell_m = cell_map(list_of_coords, cell_type_list, image, dilation)
-
-    fib_mape = fibs_map(executed_fibs['skel_labels_pruned'])
-
+    
+        
+    cell_m = cell_map (list_of_coords, cell_type_list, image, dilation)
+    
+    
+    fib_mape = fibs_map (executed_fibs['skel_labels_pruned'])
+    
+    
     visual_combin = fib_mape + cell_m
-
-    visual = pict(visual_combin, size)
-
+    
+    
+    visual = pict (visual_combin, size)
+    
+    
     return (visual)
